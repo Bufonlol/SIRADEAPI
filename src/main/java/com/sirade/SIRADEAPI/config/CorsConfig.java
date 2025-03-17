eb1655340214449621986e28b2ffed3b82a01fb1
Bufonlol <orkurito868@gmail.com>
2025-03-17 03:22:06 -0600

    QUINTO COMMIT AND PUSH

--- src/main/java/com/sirade/SIRADEAPI/config/CorsConfig.java
+++ src/main/java/com/sirade/SIRADEAPI/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.miapp.config;
+package com.sirade.SIRADEAPI.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
