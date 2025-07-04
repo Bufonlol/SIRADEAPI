37d96ae9c70c7bf744cfe62da39a8882f0fdfa42
Alan Pro <orkurito868@gmail.com>
2025-07-04 06:56:43 -0600

    marico cors

--- src/main/java/com/sirade/SIRADEAPI/config/CorsConfig.java
+++ src/main/java/com/sirade/SIRADEAPI/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://id-preview--5341bf68-42ab-4af9-ba52-dd9e41fde1ae.lovable.app")
+                        .allowedOrigins(
+                                "https://id-preview--5341bf68-42ab-4af9-ba52-dd9e41fde1ae.lovable.app",
+                                "https://id-preview--2559bef9-c5b5-4b38-b540-94f2dca4981b.lovable.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true); // HABILITAR ENVÍO DE COOKIES O HEADERS COMO AUTH
@@ -22,4 +25,3 @@ public class CorsConfig implements WebMvcConfigurer {
         };
     }
 }
-
